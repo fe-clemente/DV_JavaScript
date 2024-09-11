88d8e4c48cb001eaf60c49eb4d6b144e3cc6715d
Fernando Clemente <fe-clemente@hotmail.com>
2024-09-10 21:25:59 -0300

    256552

--- 2_Java_OO/2-solucao_saque2.js
+++ 2_Java_OO/2-solucao_saque2.js
@@ -2,7 +2,7 @@ class cliente{
     nome;
     cpf;  
   }
-const cliente1 = new cliente();
+const cliente1 = new cliente();  // variavel mesmo nome da classe
 cliente1.nome = "Fernando";
 cliente1.cpf =32500579832;
 
@@ -16,14 +16,13 @@ const contaCorrenteFernando = new contaCorrente();
 contaCorrenteFernando.saldo = 0;
 contaCorrenteFernando.agenciaConta= 1001;
 contaCorrenteFernando.saldo = 100;
-let saque = 0;
+let saque = 150;
 let saldoTotal = contaCorrenteFernando.saldo - saque;
 
-if(saldoTotal >= 1) {
-    console.log(" Pode efetuar o saque; ");
+if(saque <= saldoTotal) {
+    contaCorrenteFernando.saldo -= saque;
+console.log(" Valor sacado R$ ", saldoTotal);
 }else {
-    console.log(" negado ");
+    console.log(" Valor Negado para Saque ");
 }
-
-
 
