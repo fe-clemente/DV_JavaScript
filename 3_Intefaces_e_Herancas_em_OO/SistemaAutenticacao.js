bc8289ae7d93a18c5f25ee1afaded9c44fac20d4
Fernando Clemente <139177777+fe-clemente@users.noreply.github.com>
2024-09-27 16:02:57 -0300

    okok

--- 3_Intefaces_e_Herancas_em_OO/SistemaAutenticacao.js
+++ 3_Intefaces_e_Herancas_em_OO/SistemaAutenticacao.js
@@ -1,6 +1,6 @@
 export class SistemaAutenticacao {
     static login(funcionario, senha) {   // estatic, verifica o funcionario e valor da senha
-        return funcionario.senha == senha;  // exoressao boleane se o que retorna, devolve para o login
+        return funcionario.senha == senha;  // expressao boleane se o que retorna, devolve para o login
 
 
     }
--- 3_Intefaces_e_Herancas_em_OO/index.js
+++ 3_Intefaces_e_Herancas_em_OO/index.js
@@ -1,14 +1,29 @@
-import { Gerente } from "./Funcionario/Gerente.js";
-import { Diretor } from "./Funcionario/Diretor.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+import { Cliente } from "./Cliente.js";
 
 
 
 const diretor = new Diretor("Rodrigo", 1000000, 123456789);
 diretor.cadastrarSenha("926695");
+
+
+
 const gerente = new Gerente("Ricardo", 50000, 889486415);
+gerente.cadastrarSenha("444747");
 
 
-const estaLogado = SistemaAutenticacao.login(diretor, "9266");// const esta logado é retorno do  que a funcao SistemaAuten.. der.
+const cliente = new Cliente ("Selma", 5666, 88858410)
+cliente.cadastrarSenha("465")
 
-console.log(estaLogado);
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "926695");  // const esta logado é retorno do  que a funcao SistemaAuten.. der.
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "444747");
+
+const clienteEstalogado = SistemaAutenticacao.loging(cliente, "456");
+
+
+console.log(gerenteEstaLogo, gerenteEstaLoga);
+
+
+
